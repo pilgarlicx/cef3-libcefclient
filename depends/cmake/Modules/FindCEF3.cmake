86224950f32d5d43bd31e3c060dff66ce5796f97
uwydoc <uwydoc@gmail.com>
2016-04-09 23:13:31 +0800

    Improve FindCEF3.cmake to respect CEF3DIR, whether specified via cmake command line or as environment variable

--- depends/cmake/Modules/FindCEF3.cmake
+++ depends/cmake/Modules/FindCEF3.cmake
@@ -1,15 +1,36 @@
 # CEF3 library import helper
-function(FindCEF3 package)
-    set(${package}_FOUND ON PARENT_SCOPE)
-    get_filename_component(${package}_HOME
-        ${CMAKE_CURRENT_LIST_DIR}/../../CEF3 ABSOLUTE)
-    set(${package}_INCLUDE_DIRS ${${package}_HOME}/include PARENT_SCOPE)
-    set(${package}_LIBRARIES
-        libcef.lib
-        debug ${${package}_HOME}/lib/libcef_dll_wrapperd.lib
-        optimized ${${package}_HOME}/lib/libcef_dll_wrapper.lib
-        PARENT_SCOPE)
-    set(${package}_LIBRARY_DIRS ${${package}_HOME}/lib PARENT_SCOPE)
-endfunction(FindCEF3)
 
-FindCEF3(CEF3)
+if(MSVC)
+    # static runtime linking('/MT') required to link with CEF3
+    include(${CMAKE_CURRENT_LIST_DIR}/../configure_msvc_runtime.cmake)
+    set(MSVC_RUNTIME "static")
+    configure_msvc_runtime()
+endif()
+
+if(NOT CEF3DIR AND NOT "$ENV{CEF3DIR}")
+    get_filename_component(CEF3DIR ${CMAKE_CURRENT_LIST_DIR}/../../CEF3 ABSOLUTE)
+    set(CEF3_INCLUDE_DIRS ${CEF3DIR}/include)
+    if(WIN32)
+        set(CEF3_LIBRARIES
+            libcef.lib
+            debug ${CEF3DIR}/lib/libcef_dll_wrapperd.lib
+            optimized ${CEF3DIR}/lib/libcef_dll_wrapper.lib)
+        set(CEF3_LIBRARY_DIRS ${CEF3DIR}/lib)
+    endif()
+else()
+    find_path(CEF3_INCLUDE_DIR include/cef_app.h
+        HINTS CEF3DIR ENV CEF3DIR)
+    find_library(LIBCEF_LIBRARY libcef
+        HINTS CEF3DIR ENV CEF3DIR)
+    find_library(LIBCEF_DLL_WRAPPER_LIBRARY libcef_dll_wrapper
+        HINTS CEF3DIR ENV CEF3DIR)
+    if(CEF3_INCLUDE_DIR AND LIBCEF_LIBRARY AND LIBCEF_DLL_WRAPPER_LIBRARY)
+        set(CEF3_INCLUDE_DIRS "${CEF3_INCLUDE_DIR}")
+        set(CEF3_LIBRARIES "${LIBCEF_LIBRARY}" "${LIBCEF_DLL_WRAPPER_LIBRARY}")
+    endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(CEF3 DEFAULT_MSG
+    CEF3_INCLUDE_DIRS CEF3_LIBRARIES)
